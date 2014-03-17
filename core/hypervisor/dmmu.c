43a045d5d4ed4d044a6b73432646392305a6cd64
Hamed Nemati <hnnemati@csc.kth.se>
2014-03-17 15:48:58 +0100

    Integration with test, tlb flushes

--- core/hypervisor/dmmu.c
+++ core/hypervisor/dmmu.c
@@ -181,6 +181,11 @@ int dmmu_create_L1_pt(addr_t l1_base_pa_add)
 	uint32_t ph_block;
 	int i;
 
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	/*Check that the guest does not override the physical addresses outside its range */
 	// TODO, where we take the guest assigned physical memory?
 	if (!guest_pa_range_checker(l1_base_pa_add, 4 * PAGE_SIZE))
@@ -256,6 +261,11 @@ int dmmu_create_L1_pt(addr_t l1_base_pa_add)
 	get_bft_entry_by_block_idx(ph_block + 2)->type = PAGE_INFO_TYPE_L1PT;
 	get_bft_entry_by_block_idx(ph_block + 3)->type = PAGE_INFO_TYPE_L1PT;
 
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	return 0;
 }
 
@@ -271,6 +281,11 @@ uint32_t dmmu_map_L1_section(addr_t va, addr_t sec_base_add, uint32_t attrs)
 	uint32_t l1_desc_va_add;
 	uint32_t l1_desc_pa_add;
 	uint32_t ap;
+
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
 
 	/*Check that the guest does not override the virtual addresses used by the hypervisor */
 	// HAL_VIRT_START is usually 0xf0000000, where the hypervisor code/data structures reside
@@ -343,6 +358,12 @@ uint32_t dmmu_map_L1_section(addr_t va, addr_t sec_base_add, uint32_t attrs)
 		}
 		*((uint32_t *) l1_desc_va_add) = l1_desc;
 	}
+
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	return 0;
 }
 
@@ -357,6 +378,11 @@ uint32_t dmmu_unmap_L1_pageTable_entry(addr_t va)
 	uint32_t l1_desc_va_add;
 	uint32_t l1_desc;
 	uint32_t l1_type;
+
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
 
 	/*Check that the guest does not override the virtual addresses used by the hypervisor */
 	// HAL_VIRT_START is usually 0xf0000000, where the hypervisor code/data structures reside
@@ -395,7 +421,7 @@ uint32_t dmmu_unmap_L1_pageTable_entry(addr_t va)
 		*((uint32_t *) l1_desc_va_add) = UNMAP_L1_ENTRY(l1_desc);
 	}
 	// We are unmapping a section
-	if ((l1_type == 2) && (((l1_sec_t *) (&l1_desc))->secIndic == 0)) {
+	else if ((l1_type == 2) && (((l1_sec_t *) (&l1_desc))->secIndic == 0)) {
 		l1_sec_t *l1_sec_desc = (l1_sec_t *) (&l1_desc);
 		uint32_t ap = GET_L1_AP(l1_sec_desc);
 		int sec_idx;
@@ -513,6 +539,11 @@ void create_L2_pgtype_update(uint32_t l2_base_pa_add)
 
 uint32_t dmmu_create_L2_pt(addr_t l2_base_pa_add)
 {
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
 	uint32_t l2_desc;
@@ -561,6 +592,12 @@ uint32_t dmmu_create_L2_pt(addr_t l2_base_pa_add)
 		create_L2_pgtype_update(l2_base_pa_add);
 	} else
 		return ERR_MMU_SANITY_CHECK_FAILED;
+
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	return 0;
 }
 
@@ -575,6 +612,11 @@ int dmmu_l1_pt_map(addr_t va, addr_t l2_base_pa_add, uint32_t attrs)
 	uint32_t l1_desc_va_add;
 	uint32_t l1_desc;
 	uint32_t page_desc;
+
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
 
 	// HAL_VIRT_START is usually 0xf0000000, where the hypervisor code/data structures reside
 	/*Check that the guest does not override the virtual addresses used by the hypervisor */
@@ -622,6 +664,12 @@ int dmmu_l1_pt_map(addr_t va, addr_t l2_base_pa_add, uint32_t attrs)
 	// Updating memory with the new descriptor
 	l1_desc = CREATE_L1_PT_DESC(l2_base_pa_add, attrs);
 	*((uint32_t *) l1_desc_va_add) = l1_desc;
+
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	return 0;
 }
 
@@ -634,6 +682,11 @@ int dmmu_l1_pt_map(addr_t va, addr_t l2_base_pa_add, uint32_t attrs)
 int dmmu_l2_map_entry(addr_t l2_base_pa_add, uint32_t l2_idx,
 		      addr_t page_pa_add, uint32_t attrs)
 {
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
 	uint32_t l2_desc;
@@ -686,6 +739,11 @@ int dmmu_l2_map_entry(addr_t l2_base_pa_add, uint32_t l2_idx,
 	l2_desc = CREATE_L2_DESC(page_pa_add, attrs);
 	*((uint32_t *) l2_desc_va_add) = l2_desc;
 
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	return 0;
 }
 
@@ -694,6 +752,11 @@ int dmmu_l2_map_entry(addr_t l2_base_pa_add, uint32_t l2_idx,
  *  -------------------------------------------------------------------*/
 int dmmu_l2_unmap_entry(addr_t l2_base_pa_add, uint32_t l2_idx)
 {
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
 	uint32_t l2_desc;
@@ -728,6 +791,11 @@ int dmmu_l2_unmap_entry(addr_t l2_base_pa_add, uint32_t l2_idx)
 	l2_desc = UNMAP_L2_ENTRY(l2_desc);
 	*((uint32_t *) l2_desc_va_add) = l2_desc;
 
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	return 0;
 }
 
@@ -736,6 +804,11 @@ int dmmu_l2_unmap_entry(addr_t l2_base_pa_add, uint32_t l2_idx)
  *  -------------------------------------------------------------------*/
 int dmmu_unmap_L2_pt(addr_t l2_base_pa_add)
 {
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	uint32_t l2_desc_pa_add;
 	uint32_t l2_desc_va_add;
 	uint32_t l2_desc;
@@ -782,6 +855,11 @@ int dmmu_unmap_L2_pt(addr_t l2_base_pa_add)
 	//Changing the type of the L2 page table to data page
 	bft_entry->type = PAGE_INFO_TYPE_DATA;
 
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	return 0;
 }
 
@@ -791,6 +869,11 @@ int dmmu_unmap_L2_pt(addr_t l2_base_pa_add)
 //#define SW_DEBUG
 int dmmu_switch_mm(addr_t l1_base_pa_add)
 {
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	int i;
 	uint32_t ph_block;
 
@@ -839,6 +922,11 @@ int dmmu_switch_mm(addr_t l1_base_pa_add)
  *  ------------------------------------------------------------------- */
 int dmmu_unmap_L1_pt(addr_t l1_base_pa_add)
 {
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
+
 	uint32_t l1_idx, pt_idx, sec_idx;
 	uint32_t l1_desc;
 	uint32_t l1_desc_va_add;
@@ -915,6 +1003,11 @@ int dmmu_unmap_L1_pt(addr_t l1_base_pa_add)
 	get_bft_entry_by_block_idx(ph_block + 1)->type = PAGE_INFO_TYPE_DATA;
 	get_bft_entry_by_block_idx(ph_block + 2)->type = PAGE_INFO_TYPE_DATA;
 	get_bft_entry_by_block_idx(ph_block + 3)->type = PAGE_INFO_TYPE_DATA;
+
+	isb();
+	mem_mmu_tlb_invalidate_all(TRUE, TRUE);
+	mem_cache_invalidate(TRUE, TRUE, TRUE);	//instr, data, writeback
+	mem_cache_set_enable(TRUE);
 
 	return 0;
 }

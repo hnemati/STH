c5d5a65d3dd85a44c6eb37b1fb9642c032c8f4e2
viktor <viktordo@sics.se>
2014-06-19 16:28:05 +0200

    No optimization for trusted binary as crypto stop working, pmd set to RO/RW for each mapping

--- core/hw/cpu/arm/arm_common/start.c
+++ core/hw/cpu/arm/arm_common/start.c
@@ -3,14 +3,14 @@
 extern virtual_machine *curr_vm;
 
 void start(){
-    asm ("mov LR, %0      \n\t" :: "r"(curr_vm->config->guest_entry_point));
+    asm volatile ("mov LR, %0      \n\t" :: "r"(curr_vm->config->guest_entry_point));
 #ifdef LINUX
     /*Prepare r0 r1 and r2 for linux boot */
-    asm ("mov r1, %0      \n\t" :: "r"(LINUX_ARCH_ID));
-    asm ("mov r2, %0      \n\t" :: "r"(curr_vm->config->guest_entry_point - 0x10000 + 0x100));
-    asm ("mov r0, #0 \n\t");
+    asm volatile("mov r1, %0      \n\t" :: "r"(LINUX_ARCH_ID));
+    asm volatile("mov r2, %0      \n\t" :: "r"(curr_vm->config->guest_entry_point - 0x10000 + 0x100));
+    asm volatile("mov r0, #0 \n\t");
 #endif
-    asm ("MSR SPSR, #0xD0 \n\t");
-    asm ("MOVS PC, LR \n\t");
+    asm volatile("MSR SPSR, #0xD0 \n\t");
+    asm volatile("MOVS PC, LR \n\t");
 
 }

8edbe76895098859e17f9cc646bc7d4e085092d5
Roberto Guanciale <robertog@csc.kth.se>
2015-07-30 09:39:20 +0200

    Fixed UNMAP L1 SECTION

--- guests/dtest/src/main.c
+++ guests/dtest/src/main.c
@@ -87,22 +87,27 @@
 	    va = 0xf0000000;
 	res = ISSUE_DMMU_HYPERCALL(CMD_UNMAP_L1_PT_ENTRY, va, 0, 0);
 	expect(++t_id, "Unamap of a reserved va", ERR_MMU_RESERVED_VA, res);
-	   
-	    // #3: Unmapping 0xc0200000 is ok if this test is executed after the l1_map_section test, otherwise it has no effect
+	  
+	    // #2: Unmapping 0xc0200000 is ok if this test is executed after the l1_map_section test, otherwise it has no effect
 	    va = (va_base + 0x200000);
 	pa = va2pa(va_base);
 	attrs = 0x12;		// 0b1--10
 	attrs |= MMU_AP_USER_RW << MMU_SECTION_AP_SHIFT;
 	attrs =
 	    (attrs & (~0x10)) | 0xC | (HC_DOM_KERNEL << MMU_L1_DOMAIN_SHIFT);
-	 res = ISSUE_DMMU_HYPERCALL(CMD_MAP_L1_SECTION, va, pa, attrs);
-	expect(++t_id, "Mapping a valid writable page", SUCCESS, res);
 	 res = ISSUE_DMMU_HYPERCALL(CMD_UNMAP_L1_PT_ENTRY, va, 0, 0);
-	expect(t_id, "Unmapping a valid writable page", SUCCESS, res);
+	expect(++t_id, "Unmapping a valid writable page", SUCCESS, res);
 	 
-	    // #2: Unmapping 0xc0200000 has no effect, since this page is unmapped
-	    va = (va_base + 0x200000);
-	res = ISSUE_DMMU_HYPERCALL(CMD_UNMAP_L1_PT_ENTRY, va, 0, 0);
+	    // #3: Remap the section
+	    res = ISSUE_DMMU_HYPERCALL(CMD_MAP_L1_SECTION, va, pa, attrs);
+	expect(++t_id, "Mapping a valid writable page", SUCCESS, res);
+	 
+	    // #4: Unmapping 0xc0200000
+	    res = ISSUE_DMMU_HYPERCALL(CMD_UNMAP_L1_PT_ENTRY, va, 0, 0);
+	expect(++t_id, "Unmapping a valid writable page", SUCCESS, res);
+	 
+	    // #5: Unmapping 0xc0200000 has no effect, since this page is unmapped
+	    res = ISSUE_DMMU_HYPERCALL(CMD_UNMAP_L1_PT_ENTRY, va, 0, 0);
 	expect(++t_id, "Unamaping an not mapped entry",
 		ERR_MMU_ENTRY_UNMAPPED, res);
  }  void test_l2_create() 
@@ -799,7 +804,7 @@
 	attrs |= MMU_AP_USER_RO << MMU_SECTION_AP_SHIFT;
 	attrs =
 	    (attrs & (~0x10)) | 0xC | (HC_DOM_KERNEL << MMU_L1_DOMAIN_SHIFT);
-	res = ISSUE_DMMU_HYPERCALL(CMD_MAP_L1_SECTION, va, pa, attrs);
+	 res = ISSUE_DMMU_HYPERCALL(CMD_MAP_L1_SECTION, va, pa, attrs);
 	expect(++t_id, "Successful map of the new page", SUCCESS, res);
 	 uint32_t index = 0;
 	for (index = 0; index < 4096; index++)
